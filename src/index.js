ceedeb59057506a5a31112648cb223dca9501ffa
vijitha <vijithakn@gmail.com>
2020-02-06 10:27:56 +0530

    react example 1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import react and react-dome
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from 'faker';
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// create a react component
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <CommentDetail
+                    author="Vijitha"
+                    comment="This is a very good post."
+                    date="Today 11.30AM"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Hiranthi"
+                    comment="This is a very usefull post for me. Thank you"
+                    date="Today 10.00AM"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Kenuli"
+                    comment="Awsome post !"
+                    date="Yesterday 4.00PM"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Senaree"
+                    comment="Nice to see this kind of a post in this era."
+                    date="Yesterday 9.42AM"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+        </div>
+    );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// show react component
+ReactDOM.render(<App />, document.querySelector("#root"));
